2adc2cad62c3fb645fdc1a4bb3d379ce289c8046
Alex <robinson@fis.ucm.es>
2017-02-08 08:37:59 +0100

    intialized velocity_sia module.

---
+++ src/velocity_sia.f90
@@ -0,0 +1,32 @@
+module velocity_sia
+
+    implicit none
+    
+    
+    
+    
+    private
+    public :: calc_uv_sia
+    
+contains
+
+    subroutine calc_uv_sia
+        ! Calculate the depth integrated horizontal velocity field
+        ! using the SIA approximation
+        
+        implicit none
+        
+        
+        
+        
+        return
+        
+    end subroutine calc_uv_sia
+    
+    
+
+
+
+
+
+end module velocity_sia
