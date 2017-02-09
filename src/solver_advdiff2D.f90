d8aa42040c87edf7807f6acb9324b1da22dd9f8f
Alexander Robinson <robinson@fis.ucm.es>
2017-02-09 20:37:09 +0100

    Cleaning advdiff2D.

--- src/solver_advdiff2D.f90
+++ src/solver_advdiff2D.f90
@@ -2,133 +2,135 @@ module solver_advdiff2D
 
     implicit none
 
-contains 
+contains
 
-    subroutine  advdiff2D(Dfx,Dfy,advx,advy,vieuxH)
+    subroutine  advdiff2D(H,H_old,B_old,advx,advy,Dffx,Dffy)
         ! To solve the 2D adevection-diffusion equation:
-        ! dH/dt = 
-        
+        ! dH/dt =
+        ! M H = Frelax
+
     implicit none
-    
-    real,dimension(nx,ny), intent(in) :: Dfx      ! terme diffusif selon x
-    real,dimension(nx,ny), intent(in) :: Dfy      ! terme diffusif selon y
-    real,dimension(nx,ny), intent(in) :: Advx    ! terme advectif selon x
-    real,dimension(nx,ny), intent(in) :: Advy    ! terme advectif selon x
-    real,dimension(nx,ny), intent(in) :: vieuxH  ! terme advectif selon x
-    
-    
-    ! tableaux de travail. resolution M H = Frelax
-    real,dimension(nx,ny) :: crelax      ! diagnonale de M
-    real,dimension(nx,ny) :: arelax      ! sous diagonale selon x
-    real,dimension(nx,ny) :: brelax      ! sur diagonale selon x
-    real,dimension(nx,ny) :: drelax      ! sous diagonale selon y
-    real,dimension(nx,ny) :: erelax      ! sur diagonale selon y
-    real,dimension(nx,ny) :: frelax      ! vecteur
-    real,dimension(nx,ny) :: c_west      ! sur demi mailles Ux
-    real,dimension(nx,ny) :: c_east      ! sur demi mailles Ux
-    real,dimension(nx,ny) :: c_north     ! sur demi mailles Uy
-    real,dimension(nx,ny) :: c_south     !sur demi mailles Ux
-    
-    real,dimension(nx,ny) :: bdx         ! pente socle
-    real,dimension(nx,ny) :: bdy         ! pente socle
-    
-    real,dimension(nx,ny) :: hdx         ! pente epaisseur
-    real,dimension(nx,ny) :: hdy         ! pente epaisseur
-    
-    real(4) :: frdx,frdy                    ! pour calcul frelax : termes diffusion
-    real(4) :: fraxw,fraxe,frays,frayn      ! termes advection
-    real(4),dimension(nx,ny) :: deltah      ! dans calcul relax
-    real(4) :: delh                         ! dans calcul relax
-    real(4) :: testh                        ! dans calcul relax
+
+    real(4), intent(OUT) :: H(:,:)          ! new H returned from solver
+    real(4), intent(in)  :: H_old(:,:)      ! H from previous time step
+    real(4), intent(in)  :: B_old(:,:)      ! H from previous time step
+    real(4), intent(in)  :: Advx(:,:        ! Advective velocity - x direction
+    real(4), intent(in)  :: Advy(:,:        ! Advective velocity - y direction
+    real(4), intent(in)  :: Dffx(:,:)       ! Diffusive velocity - x direction
+    real(4), intent(in)  :: Dffy(:,:)       ! Diffusive velocity - y direction
+
+
+    ! Local variables
+    real(4), allocatable :: crelax(:,:)      ! diagnonale de M
+    real(4), allocatable :: arelax(:,:)      ! sous diagonale selon x
+    real(4), allocatable :: brelax(:,:)      ! sur diagonale selon x
+    real(4), allocatable :: drelax(:,:)      ! sous diagonale selon y
+    real(4), allocatable :: erelax(:,:)      ! sur diagonale selon y
+    real(4), allocatable :: frelax(:,:)      ! vecteur
+    real(4), allocatable :: c_west(:,:)      ! sur demi mailles Ux
+    real(4), allocatable :: c_east(:,:)      ! sur demi mailles Ux
+    real(4), allocatable :: c_north(:,:)     ! sur demi mailles Uy
+    real(4), allocatable :: c_south(:,:)     !sur demi mailles Ux
+
+    real(4), allocatable :: bdx(:,:)         ! Bedrock slope - x direction
+    real(4), allocatable :: bdy(:,:)         ! Bedrock slope - y direction
+    real(4), allocatable :: hdx(:,:)         ! Thickness gradient - x direction
+    real(4), allocatable :: hdy(:,:)         ! Thickness gradient - y direction
+
+    real(4) :: frdx,frdy                    ! helper diffusion terms
+    real(4) :: fraxw,fraxe,frays,frayn      ! helper advection terms
+
+    real(4), allocatable :: deltaH(:,:)     ! Change in H
+    real(4) :: delh
+    real(4) :: testh
     logical :: stopp
     integer :: ntour
     real(4) :: reste
     integer :: it1,it2,jt1,jt2           ! pour des tests d'asymétrie
-    
+
     real(4), parameter :: omega  = 2.5   ! parametre schema temporel de la resolution partie diffusion
     real(4), parameter :: mu_adv = 1.0   ! parametre schema temporel de la resolution advection
     real(4), parameter :: upwind = 1.0   ! schema spatial pour l'advection
-    
+
     integer, dimension(2) :: ijmax    ! position de maxval
     integer :: iFAIL
 
+    ! Caculate the gradients of H and bedrock
+    hdx = dx1*(H_old-eoshift(H_old,shift=-1,boundary=0.0,dim=1))
+    hdy = dx1*(H_old-eoshift(H_old,shift=-1,boundary=0.0,dim=2))
+    bdx = dx1*(B_old-eoshift(B_old,shift=-1,boundary=0.0,dim=1))
+    bdy = dx1*(B_old-eoshift(B_old,shift=-1,boundary=0.0,dim=2))
 
-! attention H et bm sont passés par le module  grisli_globaly
+    ! Initialize relaxation arrays
+    Arelax = 0.0
+    Brelax = 0.0
+    Drelax = 0.0
+    Erelax = 0.0
+    Crelax = 1.0
+    Frelax = 0.0
+    DeltaH = 0.0
 
-! calcul de bdx et hdx
-hdx(:,:)=dx1*(vieuxH(:,:)-eoshift(vieuxH(:,:),shift=-1,boundary=0.0,dim=1))
-hdy(:,:)=dx1*(vieuxH(:,:)-eoshift(vieuxH(:,:),shift=-1,boundary=0.0,dim=2))
-bdx(:,:)=dx1*(B(:,:)-eoshift(B(:,:),shift=-1,boundary=0.0,dim=1))
-bdy(:,:)=dx1*(B(:,:)-eoshift(B(:,:),shift=-1,boundary=0.0,dim=2))
+    ! Set boundary value to zero
+    H(1,:)  = 0.0  ! left border
+    H(nx,:) = 0.0  ! right border
+    H(:,1)  = 0.0  ! lower border
+    H(:,ny) = 0.0  ! upper border
 
+    ! Modify coefficients depending on method (upwind, central)
+    if (upwind .eq. 1.0) then
+        ! Upwind method
 
-! initialisations (qui feront aussi les conditions aux limites)
-Arelax(:,:) = 0.0
-Brelax(:,:) = 0.0
-Drelax(:,:) = 0.0
-Erelax(:,:) = 0.0
-Crelax(:,:) = 1.0
-Frelax(:,:) = 0.0
-DeltaH(:,:) = 0.0
+        where (Advx.ge.0.0)
+            c_west = 1.0
+            c_east = 0.0
+        elsewhere
+            c_west = 0.0
+            c_east = 1.0
+        end where
 
-! le conditions suivantes doivent être modifiées pour les grilles fines (AGRIF)
-H(1,:)  = 0.0  ! bord gauche
-H(nx,:) = 0.0  ! bord droit
-H(:,1)  = 0.0  ! bord bas
-H(:,ny) = 0.0  ! bord haut
+        where (Advy.ge.0.0)
+            c_south = 1.0
+            c_north = 0.0
+        elsewhere
+            c_south = 0.0
+            c_north = 1.0
+        end where
 
-! schema spatial
+    else if (upwind .lt. 1.0) then
+        ! Central method
 
-if (upwind.eq.1.0) then                 !schema amont
+        c_west  = 0.5
+        c_east  = 0.5
+        c_south = 0.5
+        c_north = 0.5
 
-   where (Advx(:,:).ge.0.)
-      c_west(:,:) = 1.0
-      c_east(:,:) = 0.0
-   elsewhere
-      c_west(:,:) = 0.0
-      c_east(:,:) = 1.0
-   end where
-
-   where (Advy(:,:).ge.0.0)
-      c_south(:,:) = 1.0
-      c_north(:,:) = 0.0
-   elsewhere
-      c_south(:,:) = 0.0
-      c_north(:,:) = 1.0
-   end where
-
-else if (upwind.lt.1.0) then             ! schema centre
-      c_west(:,:)  = 0.5
-      c_east(:,:)  = 0.5
-      c_south(:,:) = 0.5
-      c_north(:,:) = 0.5
-end if
+    end if
 
 ! attribution des elements des diagonales
-do j=2,ny-1
-  do i=2,nx-1
+    do j=2,ny-1
+    do i=2,nx-1
 
-!  sous diagonale en x
-     arelax(i,j)=-omega*Dtdx2*Dfx(i,j)   &               ! partie diffusive en x
-          -mu_adv*dtdx*c_west(i,j)*Advx(i,j)            ! partie advective en x
+        !  sous diagonale en x
+        arelax(i,j) = -omega*Dtdx2*Dffx(i,j)   &               ! partie diffusive en x
+                - mu_adv*dtdx*c_west(i,j)*Advx(i,j)            ! partie advective en x
 
-!  sur diagonale en x
-     brelax(i,j)=-omega*Dtdx2*Dfx(i+1,j)  &              ! partie diffusive
-          +mu_adv*dtdx*c_east(i+1,j)*Advx(i+1,j)        ! partie advective
+        !  sur diagonale en x
+        brelax(i,j) = -omega*Dtdx2*Dffx(i+1,j)  &              ! partie diffusive
+                + mu_adv*dtdx*c_east(i+1,j)*Advx(i+1,j)        ! partie advective
 
-!  sous diagonale en y
-     drelax(i,j)=-omega*Dtdx2*Dfy(i,j)   &               ! partie diffusive en y
-          -mu_adv*dtdx*c_south(i,j)*Advy(i,j)           ! partie advective en y
+        !  sous diagonale en y
+        drelax(i,j) = -omega*Dtdx2*Dffy(i,j)   &               ! partie diffusive en y
+                - mu_adv*dtdx*c_south(i,j)*Advy(i,j)           ! partie advective en y
 
-!  sur diagonale en y
-     erelax(i,j)=-omega*Dtdx2*Dfy(i,j+1)  &              ! partie diffusive
-          +mu_adv*dtdx*c_north(i,j+1)*Advy(i,j+1)       ! partie advective
+        !  sur diagonale en y
+        erelax(i,j) = -omega*Dtdx2*Dffy(i,j+1)  &              ! partie diffusive
+                + mu_adv*dtdx*c_north(i,j+1)*Advy(i,j+1)       ! partie advective
 
 
 
 ! diagonale
-     crelax(i,j)=omega*Dtdx2*((Dfx(i+1,j)+Dfx(i,j))  &                         ! partie diffusive en x
-                             +(Dfy(i,j+1)+Dfy(i,j)))                           ! partie diffusive en y
+     crelax(i,j)=omega*Dtdx2*((Dffx(i+1,j)+Dffx(i,j))  &                         ! partie diffusive en x
+                             +(Dffy(i,j+1)+Dffy(i,j)))                           ! partie diffusive en y
      crelax(i,j)=crelax(i,j)+mu_adv*dtdx* &
                   ( (c_west(i+1,j)*Advx(i+1,j)-c_east(i,j)*Advx(i,j)) &      !partie advective en x
                   +(c_south(i,j+1)*Advy(i,j+1)-c_north(i,j)*Advy(i,j)))      !partie advective en y
@@ -137,28 +139,28 @@ do j=2,ny-1
 
 ! terme du vecteur
 
-     frdx= -Dfx(i,j) * (Bdx(i,j)  +(1.-omega)*Hdx(i,j))         &  ! partie diffusive en x
-           +Dfx(i+1,j)*(Bdx(i+1,j)+(1.-omega)*Hdx(i+1,j))
+     frdx= -Dffx(i,j) * (Bdx(i,j)  +(1.-omega)*Hdx(i,j))         &  ! partie diffusive en x
+           +Dffx(i+1,j)*(Bdx(i+1,j)+(1.-omega)*Hdx(i+1,j))
 
-     frdy= -Dfy(i,j) * (Bdy(i,j)  +(1.-omega)*Hdy(i,j))         &  ! partie diffusive en y
-           +Dfy(i,j+1)*(Bdy(i,j+1)+(1.-omega)*Hdy(i,j+1))
+     frdy= -Dffy(i,j) * (Bdy(i,j)  +(1.-omega)*Hdy(i,j))         &  ! partie diffusive en y
+           +Dffy(i,j+1)*(Bdy(i,j+1)+(1.-omega)*Hdy(i,j+1))
 
-     fraxw= -c_west(i,j)*  Advx(i,j) * vieuxH(i-1,j)           &  ! partie advective en x 
-            +c_west(i+1,j)*Advx(i+1,j)*vieuxH(i,j)                ! venant de l'west
+     fraxw= -c_west(i,j)*  Advx(i,j) * H_old(i-1,j)           &  ! partie advective en x
+            +c_west(i+1,j)*Advx(i+1,j)*H_old(i,j)                ! venant de l'west
 
-     fraxe= -c_east(i,j) * Advx(i,j) * vieuxH(i,j)             &  ! partie advective en x 
-            +c_east(i+1,j)*Advx(i+1,j)*vieuxH(i+1,j)              ! venant de l'est
+     fraxe= -c_east(i,j) * Advx(i,j) * H_old(i,j)             &  ! partie advective en x
+            +c_east(i+1,j)*Advx(i+1,j)*H_old(i+1,j)              ! venant de l'est
 
-     frays= -c_south(i,j) * Advy(i,j) * vieuxH(i,j-1)          &  ! partie advective en y
-            +c_south(i,j+1)*Advy(i,j+1)*vieuxH(i,j)               ! venant du sud
+     frays= -c_south(i,j) * Advy(i,j) * H_old(i,j-1)          &  ! partie advective en y
+            +c_south(i,j+1)*Advy(i,j+1)*H_old(i,j)               ! venant du sud
 
-     frayn= -c_north(i,j) * Advy(i,j) * vieuxH(i,j)            &  ! partie advective en y
-            +c_north(i,j+1)*Advy(i,j+1)*vieuxH(i,j+1)             ! venant du nord
-
-
+     frayn= -c_north(i,j) * Advy(i,j) * H_old(i,j)            &  ! partie advective en y
+            +c_north(i,j+1)*Advy(i,j+1)*H_old(i,j+1)             ! venant du nord
 
 
-     frelax(i,j)=vieuxH(i,j) + dt*(bm(i,j)-bmelt(i,j)) + dtdx*(frdx+frdy) &
+
+
+     frelax(i,j)=H_old(i,j) + dt*(bm(i,j)-bmelt(i,j)) + dtdx*(frdx+frdy) &
                  + (1.-mu_adv)*dtdx*((fraxw+fraxe)+(frays+frayn))
 
 
@@ -191,7 +193,7 @@ end do
 H(:,:)=H(:,:)-deltaH(:,:)
 
 ! critere d'arret:
-! ----------------         
+! ----------------
 
 delh=0
 
@@ -218,7 +220,3 @@ return
 end subroutine advdiff2D
 
 end module solver_advdiff2D
-
-
-
-
