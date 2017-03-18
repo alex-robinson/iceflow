d1220fd8983aec85aef03a4c6458f9c8dddeb093
Alex <robinson@fis.ucm.es>
2017-03-18 09:03:40 +0100

    added mixing

---
+++ src/mixing.f90
@@ -0,0 +1,37 @@
+module mixing
+    ! Mix variables according to hybrid solver assumptions
+    
+    implicit none
+    
+    
+    
+    
+    private
+    public :: mix_var
+    public :: mix_define_f_ssa
+    
+contains
+
+    subroutine mix_var()
+        
+        implicit none
+        
+        
+        
+        
+        return
+        
+    end subroutine mix_var
+
+    subroutine mix_define_f_ssa()
+       
+        implicit none
+       
+       
+        return
+       
+    end subroutine mix_define_f_ssa
+       
+
+
+end module mixing
