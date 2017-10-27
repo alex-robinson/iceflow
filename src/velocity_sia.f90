81726d82591196cda10e7c86168bc7ceb66b2884
Alexander Robinson <robinson@fis.ucm.es>
2017-10-27 10:22:04 +0200

    Fixed bugs in sia calcs.

--- src/velocity_sia.f90
+++ src/velocity_sia.f90
@@ -1,40 +1,43 @@
 module velocity_sia
     
-    use iceflow_types
+    !use iceflow_types
     
     implicit none
     
-    
-    
-    
+    real(4), parameter :: rho_ice = 910.0 
+    real(4), parameter :: G       = 9.8 
+    real(4), parameter :: rhog    = rho_ice*G 
+
     private
-    public :: calc_uv_sia
+    public :: calc_vxy_sia
     
 contains
     
-    subroutine calc_diff_sia(ux_sia,uy_sia,diff_mx,diff_my,ddx,ddy,z_srf,H_mx,H_my, &
-                                At_int_mx,At_int_my,is_float,dx,dy,e_glen)
+    subroutine calc_vxy_sia(ux_sia,uy_sia,diff_mx,diff_my,ddx,ddy,z_srf,H_mx,H_my, &
+                                ATT_int_srf_mx,ATT_int_srf_my,is_float,dx,dy,e_glen,ulim)
         ! Calculate the depth integrated horizontal velocity field
         ! and intermediate variables using the SIA approximation
         
         implicit none
 
         real(4), intent(OUT) :: ux_sia(:,:), uy_sia(:,:)
-        real(4), intent(OUT) :: diff_mx(:,:), diff_mx(:,:)
-        real(4), intent(OUT) :: ddx(:,:), ddx(:,:)
+        real(4), intent(OUT) :: diff_mx(:,:), diff_my(:,:)
+        real(4), intent(OUT) :: ddx(:,:), ddy(:,:)
         real(4), intent(IN) :: z_srf(:,:)
         real(4), intent(IN) :: H_mx(:,:), H_my(:,:)
-        real(4), intent(IN) :: At_int_mx(:,:), At_int_my             ! Depth-integrated rate factor (ie, at surface) - called s2a_mx in grisli
+        real(4), intent(IN) :: ATT_int_srf_mx(:,:), ATT_int_srf_my(:,:)     ! Depth-integrated rate factor (ie, at surface) - called s2a_mx in grisli
         ! note: event. staggared values should be determined here locally
         logical, intent(IN) :: is_float(:,:)
-        real(4), intent(IN) :: dx, dy, e_glen
+        real(4), intent(IN) :: dx, dy, e_glen, ulim 
         
         ! Local variables
-        real(4), parameter :: INV_4DX = 1.0/(4.0*dx)
-        real(4), parameter :: INV_4DY = 1.0/(4.0*dy)
         real(4), allocatable :: sdx(:,:), sdy(:,:), sdx_my(:,:), sdy_mx(:,:)
         real(4), allocatable :: slope_mx(:,:), slope_my(:,:)
+        real(4) :: inv_4dx, inv_4dy 
         integer :: i, j, nx, ny
+        
+        inv_4dx = 1.0/(4.0*dx)
+        inv_4dy = 1.0/(4.0*dy)
         
         nx = size(z_srf,1)
         ny = size(z_srf,2)
@@ -87,20 +90,20 @@ contains
         
         where (.not. is_float)
         
-            ddx = slope_mx**(e_glen-1.0) * (ROG)**e_glen * H_mx**(e_glen+1.0)
+            ddx = slope_mx**(e_glen-1.0) * (rhog)**e_glen * H_mx**(e_glen+1.0)
             
             ! Calculate diffmx, for use with diffusion, multipy with H
             ! (initially without sliding component)
-            diff_mx = ddx*At_int_mx
+            diff_mx = ddx*ATT_int_srf_mx
             
             ! Calculate the depth-integrated SIA velocity (without sliding) and basal velocity
             ux_sia = diff_mx*(-sdx)
             
         elsewhere
         
-            ddx    = 0.0
-            diffmx = 0.0
-            ux_sia = 0.0
+            ddx     = 0.0
+            diff_mx = 0.0
+            ux_sia  = 0.0
             
         end where
         
@@ -108,11 +111,11 @@ contains
         
         where (.not. is_float)
         
-            ddy = slope_my**(e_glen-1.0) * (ROG)**e_glen * Hmy**(e_glen+1.0)
+            ddy = slope_my**(e_glen-1.0) * (rhog)**e_glen * H_my**(e_glen+1.0)
             
             ! Calculate diffmy, for use with diffusion, multipy with H
             ! (initially without sliding component)
-            diff_my = ddy*At_int_my
+            diff_my = ddy*ATT_int_srf_my
             
             ! Calculate the depth-integrated SIA velocity (without sliding) and basal velocity
             uy_sia = diff_my*(-sdy)
@@ -128,28 +131,28 @@ contains
         ! 5. Limit the deformational velocity =====================
         ! ajr: this needs revision as diffmx/my will subsequently be inconsistent
         ! Better: either limit slope in diff calculation, or move this outside of routine
-        !call limit_vel(vel%now%ux_sia,vel%par%ulim_sia)
-        !call limit_vel(vel%now%uy_sia,vel%par%ulim_sia)
+        call limit_vel(ux_sia,ulim)
+        call limit_vel(uy_sia,ulim)
         
         return
         
-    end subroutine calc_uv_sia
-    
-    
-    subroutine calc_vel3D_sia()
-        ! Calculate the 3D velocity field
-        ! using the SIA approximation
-        
-        implicit none
-        
-        
-        
-        
-        return
-        
-    end subroutine calc_vel3D_sia
+    end subroutine calc_vxy_sia
 
+    elemental subroutine limit_vel(u,u_lim)
+        ! Apply a velocity limit to points with an SIA contribution
+        ! (for stability)
 
+        implicit none 
+
+        real(4), intent(INOUT) :: u 
+        real(4), intent(IN)    :: u_lim
+
+        u = min(u, u_lim)
+        u = max(u,-u_lim)
+
+        return 
+
+    end subroutine limit_vel
 
 
 end module velocity_sia
